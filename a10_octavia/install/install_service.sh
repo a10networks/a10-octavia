32f41ce977907eb28d1939606230f2bb8bf002d0
omkartelee01 <omkareshwar.telee@a10networks.com>
2019-10-15 18:25:19 +0530

    updated the install service file

--- a10_octavia/install/install_service.sh
+++ a10_octavia/install/install_service.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "trying something"
+echo "Installing a10-controller-worker..."
 SYSTEMD_SCRIPT_DIR=$( cd  $(dirname "${BASH_SOURCE:=$0}") && pwd)
 cp -f "$SYSTEMD_SCRIPT_DIR/a10-controller-worker.service" /etc/systemd/system/
 chown root:root /etc/systemd/system/a10-controller-worker.service
@@ -8,3 +8,4 @@ chown root:root /etc/systemd/system/a10-controller-worker.service
 systemctl daemon-reload
 systemctl enable a10-controller-worker.service
 systemctl start a10-controller-worker.service
+echo "Completed installtion a10-controller-worker service"
