7eda73a15c080cba3c3f254d3338bc9b2fd3ff3b
omkartelee01 <omkareshwar.telee@a10networks.com>
2019-10-24 20:01:19 +0530

    support for rack vThunder devices

--- a10_octavia/controller/worker/controller_worker.py
+++ a10_octavia/controller/worker/controller_worker.py
@@ -217,6 +217,7 @@ class A10ControllerWorker(base_taskflow.BaseTaskFlowEngine):
                                                             constants.LISTENERS:
                                                                 [listener]})
 
+
         with tf_logging.DynamicLoggingListener(create_listener_tf,
                                                log=LOG):
             create_listener_tf.run()
@@ -293,6 +294,8 @@ class A10ControllerWorker(base_taskflow.BaseTaskFlowEngine):
         :returns: None
         :raises NoResultFound: Unable to find the object
         """
+        a10_conf = a10_config.A10Config()
+        self.config = a10_conf.get_conf()
         lb = self._lb_repo.get(db_apis.get_session(), id=load_balancer_id)
         if not lb:
             LOG.warning('Failed to fetch %s %s from DB. Retrying for up to '
