a4e73277e4f642d061e6f335126678b77cb69914
Hunter Thompson <hthompson@a10networks.com>
2019-12-31 23:08:04 +0000

    Updating QOL script

--- a10_octavia/contrib/adc_tree_create.sh
+++ a10_octavia/contrib/adc_tree_create.sh
@@ -1,8 +1,13 @@
-if [$1 == "delete"]:
-    openstack loadbalancer member delete mem1
+#!/bin/bash
+if [[ $1 == "delete" ]]; then
+    echo "[-] Deleting member"
+    openstack loadbalancer member delete pool1 mem1
+    echo "[-] Deleting pool"
     openstack loadbalancer pool delete pool1
+    echo "[-] Deleting listener"
     openstack loadbalancer listener delete l1
-else:
+else
     openstack loadbalancer listener create --protocol HTTP --protocol-port 8080 --name l1 lb1
-    openstack loadbalancer pool create --protocol HTTP --lb-algorithm ROUND_ROBIN --listener l1 --loadbalancer lb1 pool1
+    openstack loadbalancer pool create --protocol HTTP --lb-algorithm ROUND_ROBIN --listener l1 --name pool1
     openstack loadbalancer member create --address 10.0.0.1 --protocol-port 80 --name mem1 pool1
+fi
