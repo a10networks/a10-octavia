d5bb09273044e67a39200191c00c8ae0705935ef
Hunter Thompson <hthompson@a10networks.com>
2020-01-01 00:01:57 +0000

    Added ability overwrite of lb status

--- a10_octavia/contrib/scripts/hard_delete_adc_tree.sh
+++ a10_octavia/contrib/scripts/hard_delete_adc_tree.sh
@@ -4,4 +4,5 @@ USE octavia;
 DELETE FROM member WHERE name = "mem1";
 DELETE FROM listener WHERE name = "l1";
 DELETE FROM pool WHERE name = "pool1";
+UPDATE load_balancer SET provisioning_status = "ACTIVE" WHERE name = "lb1";
 EOF
