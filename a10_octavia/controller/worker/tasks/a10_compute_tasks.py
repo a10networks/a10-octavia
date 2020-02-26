d50d6b124055a25c4dd9e35c46c29b4ed7489d21
afrin-chakure <afrin.chakure@a10networks.com>
2020-02-26 10:00:14 +0530

    Refactored compute tasks

--- a10_octavia/controller/worker/tasks/a10_compute_tasks.py
+++ a10_octavia/controller/worker/tasks/a10_compute_tasks.py
@@ -12,75 +12,40 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-
 import time
 
-from cryptography import fernet
 from oslo_config import cfg
 from oslo_log import log as logging
-from stevedore import driver as stevedore_driver
-from taskflow import task
 from taskflow.types import failure
 
-from octavia.amphorae.backends.agent import agent_jinja_cfg
 from octavia.common import constants
 from octavia.common import exceptions
-from octavia.common.jinja import user_data_jinja_cfg
-from octavia.common import utils
-from octavia.controller.worker import amphora_rate_limit
-
+from octavia.controller.worker.tasks.compute_tasks import BaseComputeTask
 
 CONF = cfg.CONF
 LOG = logging.getLogger(__name__)
 
 
-class BaseComputeTask(task.Task):
-    """Base task to load drivers common to the tasks."""
-
+class A10BaseComputeTask(BaseComputeTask):
     def __init__(self, **kwargs):
-        super(BaseComputeTask, self).__init__(**kwargs)
-        self.compute = stevedore_driver.DriverManager(
-            namespace='octavia.compute.drivers',
-            name=CONF.a10_controller_worker.compute_driver,
-            invoke_on_load=True
-        ).driver
-        self.rate_limit = amphora_rate_limit.AmphoraBuildRateLimit()
+        super(A10BaseComputeTask, self).__init__(**kwargs)
 
 
-class ComputeCreate(BaseComputeTask):
+class ComputeCreate(A10BaseComputeTask):
     """Create the compute instance for a new amphora."""
 
-    def execute(self, amphora_id, config_drive_files=None,
-                build_type_priority=constants.LB_CREATE_NORMAL_PRIORITY,
+    def execute(self, amphora_id, build_type_priority=constants.LB_CREATE_NORMAL_PRIORITY,
                 server_group_id=None, ports=None):
-        """Create an amphora
-
-        :returns: an amphora
-        """
         ports = ports or []
         network_ids = CONF.a10_controller_worker.amp_boot_network_list[:]
-        config_drive_files = config_drive_files or {}
-        user_data = None
-        LOG.debug("Compute create execute for amphora with id ", amphora_id)
-
-        user_data_config_drive = CONF.a10_controller_worker.user_data_config_drive
-
+        LOG.debug("Compute create execute for amphora with id %s", amphora_id)
         key_name = CONF.a10_controller_worker.amp_ssh_key_name
         topology = CONF.a10_controller_worker.loadbalancer_topology
 
         try:
-            if CONF.haproxy_amphora.build_rate_limit != -1:
+            if CONF.a10_controller_worker.build_rate_limit != -1:
                 self.rate_limit.add_to_build_request_queue(
                     amphora_id, build_type_priority)
-
-            agent_cfg = agent_jinja_cfg.AgentJinjaTemplater()
-            config_drive_files['/etc/octavia/amphora-agent.conf'] = (
-                agent_cfg.build_agent_config(amphora_id, topology))
-            if user_data_config_drive:
-                udtemplater = user_data_jinja_cfg.UserDataJinjaCfg()
-                user_data = udtemplater.build_user_data_config(
-                    config_drive_files)
-                config_drive_files = None
 
             compute_id = self.compute.build(
                 name="amphora-" + amphora_id,
@@ -92,16 +57,14 @@ class ComputeCreate(BaseComputeTask):
                 sec_groups=CONF.a10_controller_worker.amp_secgroup_list,
                 network_ids=network_ids,
                 port_ids=[port.id for port in ports],
-                config_drive_files=config_drive_files,
-                user_data=user_data,
                 server_group_id=server_group_id)
 
-            LOG.debug("Server created with id:  for amphora id: %s",
+            LOG.debug("Server created with id: %s for amphora id: %s",
                       compute_id, amphora_id)
             return compute_id
 
         except Exception:
-            LOG.exception("Compute create for amphora id:  failed",
+            LOG.exception("Compute create for amphora id: %s failed",
                           amphora_id)
             raise
 
@@ -122,60 +85,7 @@ class ComputeCreate(BaseComputeTask):
             LOG.exception("Reverting compute create failed")
 
 
-class CertComputeCreate(ComputeCreate):
-    def execute(self, amphora_id, server_pem,
-                build_type_priority=constants.LB_CREATE_NORMAL_PRIORITY,
-                server_group_id=None, ports=None):
-        """Create an amphora
-
-        :returns: an amphora
-        """
-
-        # load client certificate
-        with open(CONF.a10_controller_worker.client_ca, 'r') as client_ca:
-            ca = client_ca.read()
-
-        key = utils.get_six_compatible_server_certs_key_passphrase()
-        fer = fernet.Fernet(key)
-        config_drive_files = {
-            '/etc/octavia/certs/server.pem': fer.decrypt(server_pem),
-            '/etc/octavia/certs/client_ca.pem': ca}
-        return super(CertComputeCreate, self).execute(
-            amphora_id, config_drive_files=config_drive_files,
-            build_type_priority=build_type_priority,
-            server_group_id=server_group_id, ports=ports)
-
-
-class DeleteAmphoraeOnLoadBalancer(BaseComputeTask):
-    """Delete the amphorae on a load balancer.
-
-    Iterate through amphorae, deleting them
-    """
-
-    def execute(self, loadbalancer):
-        for amp in loadbalancer.amphorae:
-            # The compute driver will already handle NotFound
-            try:
-                self.compute.delete(amp.compute_id)
-            except Exception:
-                LOG.exception("Compute delete for amphora id:  failed",
-                              amp.id)
-                raise
-
-
-class ComputeDelete(BaseComputeTask):
-    def execute(self, amphora):
-        LOG.debug("Compute Delete execute for amphora with id ", amphora.id)
-
-        try:
-            self.compute.delete(amphora.compute_id)
-        except Exception:
-            LOG.exception("Compute delete for amphora id:  failed",
-                          amphora.id)
-            raise
-
-
-class ComputeActiveWait(BaseComputeTask):
+class ComputeActiveWait(A10BaseComputeTask):
     """Wait for the compute driver to mark the amphora active."""
 
     def execute(self, compute_id, amphora_id):
@@ -187,7 +97,7 @@ class ComputeActiveWait(BaseComputeTask):
         for i in range(CONF.a10_controller_worker.amp_active_retries):
             amp, fault = self.compute.get_amphora(compute_id)
             if amp.status == constants.ACTIVE:
-                if CONF.haproxy_amphora.build_rate_limit != -1:
+                if CONF.a10_controller_worker.build_rate_limit != -1:
                     self.rate_limit.remove_from_build_req_queue(amphora_id)
                 return amp
             elif amp.status == constants.ERROR:
@@ -197,7 +107,7 @@ class ComputeActiveWait(BaseComputeTask):
         raise exceptions.ComputeWaitTimeoutException(id=compute_id)
 
 
-class NovaServerGroupCreate(BaseComputeTask):
+class NovaServerGroupCreate(A10BaseComputeTask):
     def execute(self, loadbalancer_id):
         """Create a server group by nova client api
 
@@ -210,7 +120,7 @@ class NovaServerGroupCreate(BaseComputeTask):
         name = 'octavia-lb-' + loadbalancer_id
         server_group = self.compute.create_server_group(
             name, CONF.a10_nova.anti_affinity_policy)
-        LOG.debug("Server Group created with id:  for load balancer id: "
+        LOG.debug("Server Group created with id: %s  for load balancer id: %s"
                   "", server_group.id, loadbalancer_id)
         return server_group.id
 
@@ -220,7 +130,7 @@ class NovaServerGroupCreate(BaseComputeTask):
         :param result: here it refers to server group id
         """
         server_group_id = result
-        LOG.warning("Reverting server group create with id:",
+        LOG.warning("Reverting server group create with id: %s",
                     server_group_id)
         try:
             self.compute.delete_server_group(server_group_id)
@@ -229,11 +139,3 @@ class NovaServerGroupCreate(BaseComputeTask):
                       "still be in use for server group: %(sg)s due to "
                       "error: %(except)s",
                       {'sg': server_group_id, 'except': e})
-
-
-class NovaServerGroupDelete(BaseComputeTask):
-    def execute(self, server_group_id):
-        if server_group_id is not None:
-            self.compute.delete_server_group(server_group_id)
-        else:
-            return
