cc9cdfb650b99f6bb8839b74d4cc8ebb8bd4d030
afrin-chakure <afrin.chakure@a10networks.com>
2020-03-05 19:01:20 +0530

    Modified reference in unit test case

--- a10_octavia/tests/unit/controller/worker/tasks/test_virtual_server_tasks.py
+++ a10_octavia/tests/unit/controller/worker/tasks/test_virtual_server_tasks.py
@@ -1,7 +1,7 @@
 import mock
 import uuid
 
-from a10_octavia.controller.worker.tasks.virtual_server_tasks import CreateVitualServerTask, DeleteVitualServerTask
+from a10_octavia.controller.worker.tasks.virtual_server_tasks import CreateVirtualServerTask, DeleteVirtualServerTask
 from a10_octavia.tests.test_base import TestBase
 
 USERNAME = "user"
@@ -15,6 +15,6 @@ class TestCreateVirtualServerTask(TestBase):
         self.lb_id = str(uuid.uuid4())
 
     def test_create(self):
-        target = CreateVitualServerTask()
+        target = CreateVirtualServerTask()
         actual = target.execute(self.lb_id, self.lb_mock, self.vthunder_mock)
         self.assertEqual(self.lb_id, actual["loadbalancers"][0]["id"])
