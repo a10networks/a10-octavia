cef884600cddcc0d41cb5655fc14932232e1beb3
afrin-chakure <afrin.chakure@a10networks.com>
2020-04-15 11:54:03 +0000

    Added check partition collision for rack

--- a10_octavia/common/utils.py
+++ a10_octavia/common/utils.py
@@ -19,7 +19,8 @@
 import netaddr
 from oslo_log import log as logging
 
-from a10_octavia.common import a10constants
+from octavia.common import exceptions
+
 from a10_octavia.common import data_models
 
 LOG = logging.getLogger(__name__)
@@ -27,9 +28,7 @@ LOG = logging.getLogger(__name__)
 
 def validate_ipv4(address):
     """Validates for IP4 address format"""
-    if not netaddr.valid_ipv4(address, netaddr.core.INET_PTON):
-        return False
-    return True
+    return netaddr.valid_ipv4(address, netaddr.core.INET_PTON)
 
 
 def validate_params(rack_info):
@@ -48,6 +47,17 @@ def validate_params(rack_info):
     return False
 
 
+def check_partition_collision(rack_device):
+    ip_part_set = set()
+    for rack in rack_device.values():
+        candidate = rack.ip_address + rack.partition
+        if candidate in ip_part_set:
+            return False
+        else:
+            ip_part_set.add(candidate)
+    return True
+
+
 def convert_to_rack_vthunder_conf(rack_list):
     """ Validates for all vthunder nouns for rack devices
         configurations.
@@ -59,9 +69,12 @@ def convert_to_rack_vthunder_conf(rack_list):
             validation_flag = validate_params(rack_device)
             if validation_flag:
                 rack_device['undercloud'] = True
-                if rack_device['partition'] is None:
-                    rack_device['partition'] = a10constants.SHARED_PARTITION
+                if 'partition' not in rack_device or not rack_device['partition']:
+                    rack_device['partition'] = 'shared'
                 vthunder_conf = data_models.VThunder(**rack_device)
+                if rack_dict.get(rack_device['project_id']):
+                    raise exceptions.ValidationException(detail=_('Supplied duplicate project_id ' +
+                                                                  rack_device['project_id'] + ' in [rack_vthunder]'))
                 rack_dict[rack_device['project_id']] = vthunder_conf
             else:
                 LOG.warning('Invalid definition of rack device for '
@@ -71,4 +84,9 @@ def convert_to_rack_vthunder_conf(rack_list):
         LOG.error("Invalid definition of rack device in configuration file."
                   "The Loadbalancer you create shall boot as overcloud."
                   "Check attribute: " + str(err))
-    return rack_dict
+
+    if check_partition_collision(rack_dict):
+        return rack_dict
+    else:
+        raise exceptions.ValidationException(
+            detail=_('Supplied duplicate partition in config file'))
