00b2c77872b9cb719e0de646a51dd3c2dced4afa
Omkar Telee <omkareshwar.telee@a10networks.com>
2020-04-24 15:23:23 +0530

    Changed constant name conflict

--- a10_octavia/tests/unit/controller/worker/tasks/test_service_group_tasks.py
+++ a10_octavia/tests/unit/controller/worker/tasks/test_service_group_tasks.py
@@ -60,10 +60,10 @@ class TestHandlerServiceGroupTasks(BaseTaskTestCase):
         mock_pool = task.PoolCreate()
         mock_pool.axapi_client = self.client_mock
         mock_pool.CONF = self.conf
-        POOL = o_data_models.Pool(id=a10constants.MOCK_POOL_ID,
+        pool = o_data_models.Pool(id=a10constants.MOCK_POOL_ID,
                                   protocol=o_constants.PROTOCOL_HTTP,
                                   lb_algorithm=o_constants.LB_ALGORITHM_SOURCE_IP)
-        mock_pool.execute(POOL, VTHUNDER)
+        mock_pool.execute(pool, VTHUNDER)
         self.client_mock.slb.service_group.create.assert_called_with(
             a10constants.MOCK_POOL_ID,
             protocol=mock.ANY,
