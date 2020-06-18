b01f65d9561fced62d2719518cd28b9c684942e8
afrin-chakure-a10 <afrin.chakure@a10networks.com>
2020-06-18 12:23:13 +0000

    Added support for accepting vrid config globally

--- a10_octavia/common/config_options.py
+++ a10_octavia/common/config_options.py
@@ -33,14 +33,16 @@ LOG = logging.getLogger(__name__)
 
 
 A10_GLOBAL_OPTS = [
-    cfg.StrOpt('vrid_floating_ip', default=None,
-               help=_('Enable VRID floating IP feature')),
     cfg.BoolOpt('enable_hierarchical_multitenancy', default=False,
                 help=_('Enable Hierarchical Multitenancy '
                        'for racked - hardware thunder devices.')),
     cfg.BoolOpt('use_parent_partition', default=False,
                 help=_('Use parent project partition on Thunder device '
                        'in hierarchical project architecture.')),
+    cfg.IntOpt('vrid', default=0,
+                help=_('VRID value')),
+    cfg.StrOpt('vrid_floating_ip', default=None,
+               help=_('Enable VRID floating IP feature')),
 ]
 
 A10_VTHUNDER_OPTS = [
