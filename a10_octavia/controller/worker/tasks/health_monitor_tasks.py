7d6efa3eb05980f3c40d9c5f6c9f9660870b2c6e
Rahul katre <rahul.katre@a10networks.com>
2020-07-14 06:55:00 +0000

    Added revert for HM delete and checked the flow

--- a10_octavia/controller/worker/tasks/health_monitor_tasks.py
+++ a10_octavia/controller/worker/tasks/health_monitor_tasks.py
@@ -67,6 +67,13 @@ class CreateAndAssociateHealthMonitor(task.Task):
             LOG.exception("Failed to associate pool to Health Monitor: %s", msg)
             raise GenericFlowException(msg)
 
+    @axapi_client_decorator
+    def revert(self, listeners, health_mon, vthunder, *args, **kwargs):
+        try:
+            self.axapi_client.slb.hm.delete(health_mon.id[:5])
+        except Exception as e:
+            LOG.exception("Failed to delete health monitor: %s", str(e))
+
 
 class DeleteHealthMonitor(task.Task):
     """Task to disassociate Health Monitor from pool and delete"""
@@ -80,13 +87,27 @@ class DeleteHealthMonitor(task.Task):
             LOG.debug("Health Monitor %s is dissociated from pool %s successfully.",
                       health_mon.id, health_mon.pool_id)
         except Exception as e:
-            LOG.warning("Failed to dissociate Health Monitor from pool: %s", str(e))
+            msg=str(e)
+            LOG.exception("Failed to dissociate Health Monitor from pool: %s", msg)
+            raise GenericFlowException(msg)
         try:
             self.axapi_client.slb.hm.delete(health_mon.id)
             LOG.debug("Health Monitor deleted successfully: %s", health_mon.id)
         except Exception as e:
-            LOG.warning("Failed to delete health monitor: %s", str(e))
-
+            msg=str(e)
+            LOG.exception("Failed to delete health monitor: %s", msg)
+            raise GenericFlowException(msg)
+    
+    @axapi_client_decorator
+    def revert(self, health_mon, vthunder, *args, **kwargs):
+        try:
+            self.axapi_client.slb.service_group.update(health_mon.pool_id,
+                                                       health_monitor=health_mon.id[:5],
+                                                       health_check_disable=0)
+            
+        except Exception as e:
+            LOG.exception("Failed to revert delete Health Monitor operation: %s", str(e))
+            
 
 class UpdateHealthMonitor(task.Task):
     """Task to update Health Monitor"""
