ac430b7cfb016a3e651fb76e5a644262cdccc574
NehaKembalkarA10 <Neha.Kembalkar@a10networks.com>
2020-09-16 16:29:48 +0530

    Added minor changes to fix a error

--- a10_octavia/controller/worker/tasks/utils.py
+++ a10_octavia/controller/worker/tasks/utils.py
@@ -64,7 +64,7 @@ def meta(lbaas_obj, key, default):
 def shared_template_modifier(template_type, template_name, device_templates):
     resource_type = template_type.split('-', 1)[1]
     resource_list_key = "{0}-list".format(resource_type)
-    if resource_list_key in device_templates['template']:
+    if resource_list_key in device_templates:
         for device_template in device_templates['template'][resource_list_key]:
             device_template_name = device_template[resource_type].get("name")
             if template_name == device_template_name:
