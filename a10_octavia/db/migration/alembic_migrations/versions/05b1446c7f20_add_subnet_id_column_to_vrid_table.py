92bcce89e5f0989f8001efb3e0c374fe227a05e7
NehaKembalkarA10 <Neha.Kembalkar@a10networks.com>
2020-11-12 12:11:17 +0000

    Incorporated review comments

--- a10_octavia/db/migration/alembic_migrations/versions/05b1446c7f20_add_subnet_id_column_to_vrid_table.py
+++ a10_octavia/db/migration/alembic_migrations/versions/05b1446c7f20_add_subnet_id_column_to_vrid_table.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '05b1446c7f20'
-down_revision = 'ea8bfcbd654c'
+down_revision = '4028e5f7a198'
 branch_labels = None
 depends_on = None
 
