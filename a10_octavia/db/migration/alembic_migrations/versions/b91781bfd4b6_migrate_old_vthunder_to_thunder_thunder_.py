551b5e5e9e33ab8cbd2252324a975b2b071194cc
afrin-chakure-a10 <afrin.chakure@a10networks.com>
2020-11-12 12:28:19 +0000

    Added data migration for project and partitions

--- a10_octavia/db/migration/alembic_migrations/versions/b91781bfd4b6_migrate_old_vthunder_to_thunder_thunder_.py
+++ a10_octavia/db/migration/alembic_migrations/versions/b91781bfd4b6_migrate_old_vthunder_to_thunder_thunder_.py
@@ -12,7 +12,7 @@ from sqlalchemy.orm import sessionmaker
 from oslo_utils import uuidutils
 
 from a10_octavia import a10_config
-from a10_octavia.db.models import Thunder, Thunder_Cluster
+from a10_octavia.db.models import Partitions, Project, Thunder_Cluster
 
 # revision identifiers, used by Alembic.
 revision = 'b91781bfd4b6'
@@ -35,12 +35,12 @@ def upgrade():
     db_engine = sa.create_engine(db_str)
     with db_engine.connect() as con:
         results = con.execute('select * from vthunders')
-        thunder = []
         thunder_cluster = []
+        partitions = []
+        project = []
         for _row in results:
-            thunder_cluster_id = uuidutils.generate_uuid()
             thunder_cluster.append(Thunder_Cluster(
-                id=thunder_cluster_id,
+                id=_row[1],
                 username=_row[5],
                 password=_row[6],
                 cluster_name=_row[3],
@@ -48,20 +48,27 @@ def upgrade():
                 topology=_row[12],
                 undercloud=_row[8]
             ))
-            thunder.append(Thunder(
-                id=uuidutils.generate_uuid(),
-                cluster_id=thunder_cluster_id,
-                vcs_device_id="",
-                management_ip_address=""
-            ))
-        sess.add_all(thunder)
+            partition_id = uuidutils.generate_uuid()
+            partitions.append(Partitions(id=partition_id,
+                                         name=_row[18],
+                                         hierarchical_multitenancy=_row[19],
+                                         created_at=_row[16],
+                                         updated_at=_row[17]))
+            project.append(Project(id=_row[10],
+                                   partition_id=partition_id,
+                                   thunder_cluster_id=_row[1],
+                                   created_at=_row[16],
+                                   updated_at=_row[17]))
         sess.add_all(thunder_cluster)
+        sess.add_all(partitions)
+        sess.add_all(project)
         sess.commit()
     sess.close()
 
 
 def downgrade():
-    sess.query(Thunder).filter().delete()
+    sess.query(Project).filter().delete()
+    sess.query(Partitions).filter().delete()
     sess.query(Thunder_Cluster).filter().delete()
     sess.commit()
     sess.close()
