be674be10f326f6d313aea1c78e5a720f737bc38
Rahul katre <rahul.katre@a10networks.com>
2020-11-19 10:57:31 +0000

    Updated the data-models tests and added unit tests for Partition and Thunder data-models

--- a10_octavia/tests/unit/common/test_data_models.py
+++ a10_octavia/tests/unit/common/test_data_models.py
@@ -12,7 +12,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import copy
 import datetime
 
 from oslo_utils import uuidutils
@@ -26,97 +25,35 @@ class TestDataModels(base.BaseTaskTestCase):
     def setUp(self):
 
         super(TestDataModels, self).setUp()
-        self.AMP_ID = uuidutils.generate_uuid()
-        self.THUDER_CLUSTER_ID = uuidutils.generate_uuid()
         self.CREATED_AT = datetime.datetime.now()
         self.UPDATED_AT = datetime.datetime.utcnow()
         self.LAST_UDP_UPDATE = datetime.datetime.utcnow()
-        self.SUBNET_ID = uuidutils.generate_uuid()
-        self.VLAN_ID = uuidutils.generate_uuid()
-        self.PORT_ID = uuidutils.generate_uuid()
-        self.DEVICE_ID = uuidutils.generate_uuid()
-        self.THUNDER_ID = uuidutils.generate_uuid()
 
-        self.AMP_obj = data_models.AmphoraMeta(
-            id=self.AMP_ID,
-            created_at=self.CREATED_AT,
-            updated_at=self.UPDATED_AT,
-            last_udp_update=self.LAST_UDP_UPDATE,
-            status="ACTIVE"
-        )
-
-        self.THUNDER_CLUSTER_obj = data_models.ThunderCluster(
-            id=self.THUDER_CLUSTER_ID,
-            created_at=self.CREATED_AT,
-            updated_at=self.UPDATED_AT,
-            username="USER1",
-            password="pwd",
-            cluster_name="cluster1",
-            cluster_ip_address="192.0.2.10",
-            undercloud=False,
-            topology="STANDALONE"
-        )
-
-        self.ETH_INT_obj = data_models.EthernetInterface(
-            interface_num=1,
-            created_at=self.CREATED_AT,
-            updated_at=self.UPDATED_AT,
-            subnet_id=self.SUBNET_ID,
-            vlan_id=self.VLAN_ID,
-            ve_ip_address="192.0.2.11",
-            port_id=self.PORT_ID
-        )
-
-        self.TRUNK_INT_obj = data_models.TrunkInterface(
-            interface_num=2,
-            created_at=self.CREATED_AT,
-            updated_at=self.UPDATED_AT,
-            subnet_id=self.SUBNET_ID,
-            vlan_id=self.VLAN_ID,
-            ve_ip_address="192.0.2.12",
-            port_id=self.PORT_ID
-        )
-
-        self.DEVICE_NET_obj = data_models.DeviceNetworkCluster(
-            id=self.DEVICE_ID,
-            created_at=self.CREATED_AT,
-            updated_at=self.UPDATED_AT,
-            thunder_id=self.THUNDER_ID,
-            ethernet_interface_num=1,
-            trunk_interface_num=2
-        )
-
-    def test_AmphoraMeta_update(self):
+    def test_AmphoraMeta(self):
 
         new_id = uuidutils.generate_uuid()
         new_created_at = self.CREATED_AT + datetime.timedelta(minutes=5)
         new_updated_at = self.UPDATED_AT + datetime.timedelta(minutes=10)
         new_last_udp_update = self.LAST_UDP_UPDATE + datetime.timedelta(minutes=5)
-        new_status = "STANDBY"
 
-        update_dict = {
+        amphora_meta_data = {
             'id': new_id,
             'created_at': new_created_at,
             'updated_at': new_updated_at,
             'last_udp_update': new_last_udp_update,
-            'status': new_status
+            'status': None
         }
-
-        test_Amp_obj = copy.deepcopy(self.AMP_obj)
 
         reference_Amp_obj = data_models.AmphoraMeta(
             id=new_id,
             created_at=new_created_at,
             updated_at=new_updated_at,
             last_udp_update=new_last_udp_update,
-            status=new_status
         )
 
-        test_Amp_obj.update(update_dict)
+        self.assertEqual(reference_Amp_obj.__dict__, amphora_meta_data)
 
-        self.assertEqual(reference_Amp_obj, test_Amp_obj)
-
-    def test_ThunderCluster_update(self):
+    def test_ThunderCluster(self):
 
         new_id = uuidutils.generate_uuid()
         new_created_at = self.CREATED_AT + datetime.timedelta(minutes=5)
@@ -128,7 +65,7 @@ class TestDataModels(base.BaseTaskTestCase):
         new_undercloud = False
         new_topology = "STANDALONE"
 
-        update_dict = {
+        thunder_cluster_data = {
             'id': new_id,
             'created_at': new_created_at,
             'updated_at': new_updated_at,
@@ -139,8 +76,6 @@ class TestDataModels(base.BaseTaskTestCase):
             'undercloud': new_undercloud,
             'topology': new_topology
         }
-
-        test_Thunder_Cluster_obj = copy.deepcopy(self.THUNDER_CLUSTER_obj)
 
         reference_Thunder_Cluster_obj = data_models.ThunderCluster(
             id=new_id,
@@ -154,11 +89,9 @@ class TestDataModels(base.BaseTaskTestCase):
             topology=new_topology
         )
 
-        test_Thunder_Cluster_obj.update(update_dict)
+        self.assertEqual(reference_Thunder_Cluster_obj.__dict__, thunder_cluster_data)
 
-        self.assertEqual(reference_Thunder_Cluster_obj, test_Thunder_Cluster_obj)
-
-    def test_Eth_Interface_update(self):
+    def test_Eth_Interface(self):
 
         new_interface_num = 2
         new_created_at = self.CREATED_AT + datetime.timedelta(minutes=5)
@@ -168,7 +101,7 @@ class TestDataModels(base.BaseTaskTestCase):
         new_ve_ip_address = "192.0.2.10"
         new_port_id = uuidutils.generate_uuid()
 
-        update_dict = {
+        ethernet_interface_data = {
             'interface_num': new_interface_num,
             'created_at': new_created_at,
             'updated_at': new_updated_at,
@@ -177,8 +110,6 @@ class TestDataModels(base.BaseTaskTestCase):
             've_ip_address': new_ve_ip_address,
             'port_id': new_port_id
         }
-
-        test_Eth_Int_obj = copy.deepcopy(self.ETH_INT_obj)
 
         reference_Eth_Int_obj = data_models.EthernetInterface(
             interface_num=new_interface_num,
@@ -190,11 +121,9 @@ class TestDataModels(base.BaseTaskTestCase):
             port_id=new_port_id
         )
 
-        test_Eth_Int_obj.update(update_dict)
+        self.assertEqual(reference_Eth_Int_obj.__dict__, ethernet_interface_data)
 
-        self.assertEqual(reference_Eth_Int_obj, test_Eth_Int_obj)
-
-    def test_Trunk_Interface_update(self):
+    def test_Trunk_Interface(self):
 
         new_interface_num = 3
         new_created_at = self.CREATED_AT + datetime.timedelta(minutes=5)
@@ -204,7 +133,7 @@ class TestDataModels(base.BaseTaskTestCase):
         new_ve_ip_address = "192.0.2.10"
         new_port_id = uuidutils.generate_uuid()
 
-        update_dict = {
+        trunk_interface_data = {
             'interface_num': new_interface_num,
             'created_at': new_created_at,
             'updated_at': new_updated_at,
@@ -213,8 +142,6 @@ class TestDataModels(base.BaseTaskTestCase):
             've_ip_address': new_ve_ip_address,
             'port_id': new_port_id
         }
-
-        test_Trunk_Int_obj = copy.deepcopy(self.TRUNK_INT_obj)
 
         reference_Trunk_Int_obj = data_models.TrunkInterface(
             interface_num=new_interface_num,
@@ -226,11 +153,9 @@ class TestDataModels(base.BaseTaskTestCase):
             port_id=new_port_id
         )
 
-        test_Trunk_Int_obj.update(update_dict)
+        self.assertEqual(reference_Trunk_Int_obj.__dict__, trunk_interface_data)
 
-        self.assertEqual(reference_Trunk_Int_obj, test_Trunk_Int_obj)
-
-    def test_DeviceNetworkCluster_update(self):
+    def test_DeviceNetworkCluster(self):
 
         new_id = uuidutils.generate_uuid()
         new_created_at = self.CREATED_AT + datetime.timedelta(minutes=5)
@@ -239,7 +164,7 @@ class TestDataModels(base.BaseTaskTestCase):
         new_ethernet_interface_num = 3
         new_trunk_interface_num = 4
 
-        update_dict = {
+        device_network_cluster_data = {
             'id': new_id,
             'created_at': new_created_at,
             'updated_at': new_updated_at,
@@ -247,8 +172,6 @@ class TestDataModels(base.BaseTaskTestCase):
             'ethernet_interface_num': new_ethernet_interface_num,
             'trunk_interface_num': new_trunk_interface_num
         }
-
-        test_Device_Net_obj = copy.deepcopy(self.DEVICE_NET_obj)
 
         reference_Device_Net_obj = data_models.DeviceNetworkCluster(
             id=new_id,
@@ -259,6 +182,59 @@ class TestDataModels(base.BaseTaskTestCase):
             trunk_interface_num=new_trunk_interface_num
         )
 
-        test_Device_Net_obj.update(update_dict)
+        self.assertEqual(reference_Device_Net_obj.__dict__, device_network_cluster_data)
 
-        self.assertEqual(reference_Device_Net_obj, test_Device_Net_obj)
+    def test_Partitions(self):
+
+        new_id = uuidutils.generate_uuid()
+        new_created_at = self.CREATED_AT + datetime.timedelta(minutes=5)
+        new_updated_at = self.UPDATED_AT + datetime.timedelta(minutes=10)
+        new_name = "partition-alpha"
+        new_hierarchical_multitenancy = "enable"
+
+        partition_data = {
+            'id': new_id,
+            'created_at': new_created_at,
+            'updated_at': new_updated_at,
+            'name': new_name,
+            'hierarchical_multitenancy': new_hierarchical_multitenancy
+        }
+
+        partition_data_obj = data_models.Partitions(
+            id=new_id,
+            name=new_name,
+            hierarchical_multitenancy=new_hierarchical_multitenancy,
+            created_at=new_created_at,
+            updated_at=new_updated_at
+        )
+
+        self.assertEqual(partition_data_obj.__dict__, partition_data)
+
+    def test_Thunder(self):
+
+        new_id = uuidutils.generate_uuid()
+        new_created_at = self.CREATED_AT + datetime.timedelta(minutes=5)
+        new_updated_at = self.UPDATED_AT + datetime.timedelta(minutes=10)
+        new_vcs_device_id = 1
+        new_management_ip_address = "10.0.0.74"
+        new_cluster_id = uuidutils.generate_uuid()
+
+        thunder_data = {
+            'id': new_id,
+            'vcs_device_id': new_vcs_device_id,
+            'management_ip_address': new_management_ip_address,
+            'cluster_id': new_cluster_id,
+            'created_at': new_created_at,
+            'updated_at': new_updated_at,
+        }
+
+        thunder_data_obj = data_models.Thunder(
+            id=new_id,
+            vcs_device_id=new_vcs_device_id,
+            management_ip_address=new_management_ip_address,
+            cluster_id=new_cluster_id,
+            created_at=new_created_at,
+            updated_at=new_updated_at
+        )
+
+        self.assertEqual(thunder_data_obj.__dict__, thunder_data)
