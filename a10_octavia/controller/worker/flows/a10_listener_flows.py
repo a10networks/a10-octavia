639cd12176d1c4a2cbddd7c69b0233c4a7ef6e82
NehaKembalkarA10 <Neha.Kembalkar@a10networks.com>
2021-01-07 12:08:36 +0000

    Added support to update connection_limit after using it in openstack cli to set

--- a10_octavia/controller/worker/flows/a10_listener_flows.py
+++ a10_octavia/controller/worker/flows/a10_listener_flows.py
@@ -132,7 +132,7 @@ class ListenerFlows(object):
             provides=constants.FLAVOR))
         update_listener_flow.add(virtual_port_tasks.ListenerUpdate(
             requires=[constants.LOADBALANCER, constants.LISTENER,
-                      a10constants.VTHUNDER, constants.FLAVOR]))
+                      a10constants.VTHUNDER, constants.FLAVOR, constants.UPDATE_DICT]))
         update_listener_flow.add(database_tasks.UpdateListenerInDB(
             requires=[constants.LISTENER, constants.UPDATE_DICT]))
         update_listener_flow.add(a10_database_tasks.
