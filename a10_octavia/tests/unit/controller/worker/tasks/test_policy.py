d18574f1f78f0749c052080d10546834cbe11b1e
Rahul Katre <rahul.katre@a10networks.com>
2021-01-15 12:48:06 +0000

    Fixed L7rule creation for FILE_TYPE type.

--- a10_octavia/tests/unit/controller/worker/tasks/test_policy.py
+++ a10_octavia/tests/unit/controller/worker/tasks/test_policy.py
@@ -35,3 +35,23 @@ class TestPolicy(base.TestCase):
         policy_util.createPolicy(L7POLICY)
         self.assertEqual(policy_util.ruleParser(L7RULE),
                          '([HTTP::path] equals "www://abc.com")')
+
+    def test_policy_rule_file_type_regex(self):
+        L7rule_file_type = o_data_models.L7Rule(id=a10constants.MOCK_L7RULE_ID,
+                                                l7policy_id=a10constants.MOCK_L7POLICY_ID,
+                                                type="FILE_TYPE", compare_type="REGEX",
+                                                value=".(html|xml)")
+        L7POLICY.l7rules = [L7rule_file_type]
+        policy_util = policy.PolicyUtil()
+        self.assertEqual(policy_util.ruleParser(L7rule_file_type),
+                         '([HTTP::uri] matches_regex ".(html|xml)")')
+
+    def test_policy_rule_file_type_equals(self):
+        L7rule_file_type = o_data_models.L7Rule(id=a10constants.MOCK_L7RULE_ID,
+                                                l7policy_id=a10constants.MOCK_L7POLICY_ID,
+                                                type="FILE_TYPE", compare_type="EQUAL_TO",
+                                                value="ccccc")
+        L7POLICY.l7rules = [L7rule_file_type]
+        policy_util = policy.PolicyUtil()
+        self.assertEqual(policy_util.ruleParser(L7rule_file_type),
+                         '([HTTP::uri] ends_with "ccccc")')
