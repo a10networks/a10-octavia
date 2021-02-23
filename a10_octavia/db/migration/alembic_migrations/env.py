8489df9a1cb7558bff34f142be2e3d410875a0d7
ytsai-a10 <ytsai@a10networks.com>
2021-02-23 17:12:42 +0800

    [STACK-2066] Database Migration Enhancement

--- a10_octavia/db/migration/alembic_migrations/env.py
+++ a10_octavia/db/migration/alembic_migrations/env.py
@@ -4,10 +4,19 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+import sqlalchemy as sa
 
-from alembic import context
+from alembic import context, command
+from alembic.script import ScriptDirectory
+from alembic.operations import Operations
+
 from a10_octavia import a10_config
 from a10_octavia.db import base_models
+
+VERSION_TABLE = 'alembic_version_a10'
+LOG_INFO = "INFO  [a10-octavia.db.migration] "
+LOG_ERR = "ERROR  [a10-octavia.db.migration] "
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -31,6 +40,51 @@ if getattr(config, 'connection', None) is None:
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
+
+def get_octavia_revision(ctx, op):
+    current_rev = 0
+    try:
+        conn = op.get_bind()
+        current_rev = conn.execute("select version_num from alembic_version").scalar()
+    except:
+        config.print_stdout("%sFailed to get octavia alembic revision", LOG_ERR)
+
+    return current_rev
+
+def a10_revision_migration(ctx):
+    """ Check a10 database revision from 'alembic_version' table
+
+    In a10-octavia v1.1 and earlier vrersions, a10-octavia use 
+    'alembic_version' table as alembic version table (which will
+    overwrite octavia alembic revisions). 
+
+    To migrate from these versions, we need to copy the revision
+    from'alembic_version' to 'alembic_version_a10'.
+
+    """
+    op = Operations(ctx)
+    octavia_rev = get_octavia_revision(ctx, op)
+    script = ScriptDirectory.from_config(config)
+    dest_rev = 0
+    try:
+        rev = script.get_revision(octavia_rev)
+        config.print_stdout("%soctavia revision %s is a10 revision", LOG_INFO, rev.revision)
+        dest_rev = rev.revision
+    except:
+        config.print_stdout("%s%s is not a10 revision", LOG_INFO, octavia_rev)
+        return
+
+    try:
+        ctx.stamp(script, dest_rev)
+        config.print_stdout("%sstamp a10_revision to %s", LOG_INFO, dest_rev)
+
+        # Since revision is copy to alembic_version_a10, remove a10 revision in octavia table.
+        # Keep a10 vision in octavia may cause issue after a10-octavia do `alembic downgrade base`
+        conn = op.get_bind()
+        conn.execute("delete from alembic_version")
+    except:
+        config.print_stdout("%smigrate version from alembic_version failed", LOG_ERR)
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
@@ -68,8 +122,19 @@ def run_migrations_online():
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            version_table=VERSION_TABLE,
+            target_metadata=target_metadata
         )
+
+        # a10-octavia revision checks
+        ctx = context.get_context()
+        a10_revision = ctx.get_current_revision()
+        if a10_revision is None:
+            config.print_stdout("%sNo a10_revision yet.", LOG_INFO)
+            a10_revision_migration(ctx=ctx)
+        else:
+            config.print_stdout("%scurrent a10_revision: %s", LOG_INFO, a10_revision)
 
         with context.begin_transaction():
             context.run_migrations()
