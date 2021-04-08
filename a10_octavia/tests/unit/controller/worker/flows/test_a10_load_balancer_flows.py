a009e89e77b42f7efe11f7bd1b3e378cf9422971
ytsai-a10 <ytsai@a10networks.com>
2021-04-08 23:19:33 +0800

    fix travis errors

--- a10_octavia/tests/unit/controller/worker/flows/test_a10_load_balancer_flows.py
+++ a10_octavia/tests/unit/controller/worker/flows/test_a10_load_balancer_flows.py
@@ -59,7 +59,7 @@ class TestLoadBalancerFlows(base.TestCase):
     def test_create_lb_flows(self, mock_net_driver):
         lb = o_data_models.LoadBalancer(id=a10constants.MOCK_LOAD_BALANCER_ID,
                                         project_id='project-vthunder')
-        (create_flow, store) = self.flows.get_delete_load_balancer_flow(lb, False)
+        (create_flow, store) = self.flows.get_delete_load_balancer_flow(lb, False, False)
         self.assertIsInstance(create_flow, flow.Flow)
 
     def test_create_lb_rack_vthunder_vlan_flow(self, mock_net_driver):
