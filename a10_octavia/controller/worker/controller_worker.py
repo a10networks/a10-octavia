e2050609ab3efcf04511c1b1bb4e238b16ce9d07
NehaKembalkarA10 <Neha.Kembalkar@a10networks.com>
2021-04-13 14:26:38 +0000

    Incorporated a review comment

--- a10_octavia/controller/worker/controller_worker.py
+++ a10_octavia/controller/worker/controller_worker.py
@@ -323,20 +323,14 @@ class A10ControllerWorker(base_taskflow.BaseTaskFlowEngine):
             deleteCompute = self._vthunder_repo.get_delete_compute_flag(db_apis.get_session(),
                                                                         vthunder.compute_id)
 
+        if cascade:
+            cascade = True
         if lb.project_id in CONF.hardware_thunder.devices:
-            if cascade:
-                (flow, store) = self._lb_flows.get_delete_rack_vthunder_load_balancer_flow(
-                    lb, deleteCompute, True)
-            else:
-                (flow, store) = self._lb_flows.get_delete_rack_vthunder_load_balancer_flow(
-                    lb, deleteCompute, False)
+            (flow, store) = self._lb_flows.get_delete_rack_vthunder_load_balancer_flow(
+                lb, deleteCompute, cascade)
         else:
-            if cascade:
-                (flow, store) = self._lb_flows.get_delete_load_balancer_flow(
-                    lb, deleteCompute, True)
-            else:
-                (flow, store) = self._lb_flows.get_delete_load_balancer_flow(
-                    lb, deleteCompute, False)
+            (flow, store) = self._lb_flows.get_delete_load_balancer_flow(
+                lb, deleteCompute, cascade)
 
         store.update({constants.LOADBALANCER: lb,
                       constants.VIP: lb.vip,
