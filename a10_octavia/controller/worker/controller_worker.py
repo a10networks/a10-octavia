152e9331e848d493848fcfef5cf231fe731f6746
NehaKembalkarA10 <Neha.Kembalkar@a10networks.com>
2021-08-30 08:58:55 +0000

    Added default_flavor_id config option and making its database entry when used

--- a10_octavia/controller/worker/controller_worker.py
+++ a10_octavia/controller/worker/controller_worker.py
@@ -395,6 +395,11 @@ class A10ControllerWorker(base_taskflow.BaseTaskFlowEngine):
                         '60 seconds.', 'load_balancer', load_balancer_id)
             raise db_exceptions.NoResultFound
 
+        if not lb.flavor_id and CONF.a10_global.default_flavor_id:
+            flavor_id = CONF.a10_global.default_flavor_id
+        else:
+            flavor_id = lb.flavor_id
+
         store = {constants.LOADBALANCER_ID: load_balancer_id,
                  constants.VIP: lb.vip,
                  constants.BUILD_TYPE_PRIORITY:
@@ -405,7 +410,8 @@ class A10ControllerWorker(base_taskflow.BaseTaskFlowEngine):
         topology = CONF.a10_controller_worker.loadbalancer_topology
 
         store[constants.UPDATE_DICT] = {
-            constants.TOPOLOGY: topology
+            constants.TOPOLOGY: topology,
+            constants.FLAVOR_ID: flavor_id
         }
 
         ctx_flags = [False]
